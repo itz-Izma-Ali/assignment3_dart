4f38d8013a4129a9d9d44301b3f3d9141469ce02
itz-Izma-Ali <izma91700@gmail.com>
2024-09-07 23:25:12 +0500

    studentInfo

---
+++ qno3.dart
@@ -0,0 +1,14 @@
+void main() {
+  List studentInfo = [
+    'Izma Ali', 
+    'Software Engineering', 
+    'BSE-22F-037',
+    'A',  
+    '88.5' 
+  ];
+  print("Name: " + studentInfo[0]);
+  print("Class: " + studentInfo[1]);
+  print("Roll No: " + studentInfo[2]);
+  print("Grade: " + studentInfo[3]);
+  print("Percentage: " + studentInfo[4]+ "%");
+}
