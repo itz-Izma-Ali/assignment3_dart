110b729a413b1765e233c833a0c6865af10a679a
itz-Izma-Ali <izma91700@gmail.com>
2024-09-07 23:38:08 +0500

    eligible user

---
+++ qno8.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
+
+  usersEligibility.removeWhere((user) => user != 'eligible');
+  print("Eligible users: $usersEligibility");
+}
