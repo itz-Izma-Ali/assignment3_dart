4bf47229834df88dc7c8c779613c7fd19be54138
itz-Izma-Ali <izma91700@gmail.com>
2024-09-07 23:34:02 +0500

    original and reversed list

---
+++ qno6.dart
@@ -0,0 +1,14 @@
+void main() {
+  var fruitlist = [
+    'mango',
+    'banana',
+    'apple',
+    'watermelon',
+    'orange',
+    'grapes'
+  ];
+  var reversedlist = List.of(fruitlist.reversed);
+  print("original list: $fruitlist");
+  print("reversed list: $reversedlist");
+
+}
